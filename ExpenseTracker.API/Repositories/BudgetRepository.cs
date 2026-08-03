5bddca98c52ed0336cf2dbd88ed52e5c4969ef11
Abhilash Gupta <abhilash.gupta22@gmail.com>
2026-08-03 23:59:56 +0530

    Added repository in ExpenseTracker.API

---
+++ ExpenseTracker.API/Repositories/BudgetRepository.cs
@@ -0,0 +1,21 @@
+﻿using ExpenseTracker.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.API.Repositories
+{
+    public interface IBudgetRepository
+    {
+        Task AddAsync(Budget budget);
+        Task UpdateAsync(Budget budget);
+        Task DeleteAsync(Guid id);
+        Task<Budget?> GetByIdAsync(Guid id);
+        Task<IEnumerable<Budget>> GetAllAsync();
+        Task<bool> ExistsAsync(Guid id);
+        Task<Budget?> GetBudgetAsync(int month, int year);
+        Task<bool> BudgetExistsAsync(int month, int year);
+        Task<Budget?> GetBudgetWithCategoriesAsync(Guid budgetId);
+        Task<Budget?> GetBudgetCompleteAsync(Guid budgetId);
+    }
+}
