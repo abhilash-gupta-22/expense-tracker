1a33b649f1c248a04d8640ed1e91845ce03197c5
Abhilash Gupta <abhilash.gupta22@gmail.com>
2026-08-07 00:01:46 +0530

    Implemntedt budget repository

--- ExpenseTracker.API/Repositories/BudgetRepository.cs
+++ ExpenseTracker.API/Repositories/BudgetRepository.cs
@@ -1,61 +1,111 @@
 ﻿using ExpenseTracker.Domain.Entities;
 using ExpenseTracker.Domain.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+using ExpenseTracker.API.Data;
+using Microsoft.EntityFrameworkCore;
+using ExpenseTracker.Domain.Common;
 
-namespace ExpenseTracker.API.Repositories
+namespace ExpenseTracker.API.Repositories;
+
+public class BudgetRepository : IBudgetRepository
 {
-    public class BudgetRepository : IBudgetRepository
+    private readonly ExpenseTrackerDbContext _context;
+
+    public BudgetRepository(ExpenseTrackerDbContext context)
     {
-        public Task AddAsync(Budget entity)
+        Guard.AgainstNull(context, nameof(context));
+
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Budget>> GetAllAsync()
+    {
+        return await _context.Budgets.AsNoTracking()
+            .OrderByDescending(x => x.Year)
+            .ThenByDescending(x => x.Month)
+            .ToListAsync().ConfigureAwait(false);
+    }
+
+    public async Task<Budget?> GetByIdAsync(Guid id)
+    {
+        Guard.AgainstNull(id, nameof(id));
+
+        return await _context.Budgets.AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Id == id).ConfigureAwait(false);
+    }
+
+    public async Task AddAsync(Budget entity)
+    {
+        Guard.AgainstNull(entity, nameof(entity));
+
+        await _context.Budgets.AddAsync(entity).ConfigureAwait(false);
+        await _context.SaveChangesAsync().ConfigureAwait(false);
+    }
+
+    public async Task UpdateAsync(Budget entity)
+    {
+        Guard.AgainstNull(entity, nameof(entity));
+
+        _context.Budgets.Update(entity);
+
+        await _context.SaveChangesAsync().ConfigureAwait(false);
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        Guard.AgainstNull(id, nameof(id));
+
+        var budget = await _context.Budgets.FindAsync(id).ConfigureAwait(false);
+
+        if (budget is null)
         {
-            throw new NotImplementedException();
+            return;
         }
 
-        public Task<bool> BudgetExistsAsync(int month, int year)
-        {
-            throw new NotImplementedException();
-        }
+        _context.Budgets.Remove(budget);
 
-        public Task DeleteAsync(Guid id)
-        {
-            throw new NotImplementedException();
-        }
+        await _context.SaveChangesAsync().ConfigureAwait(false);
+    }
 
-        public Task<bool> ExistsAsync(Guid id)
-        {
-            throw new NotImplementedException();
-        }
+    public async Task<bool> ExistsAsync(Guid id)
+    {
+        Guard.AgainstNull(id, nameof(id));
 
-        public Task<IEnumerable<Budget>> GetAllAsync()
-        {
-            throw new NotImplementedException();
-        }
+        return await _context.Budgets.AnyAsync(x => x.Id == id).ConfigureAwait(false);
+    }
 
-        public Task<Budget?> GetBudgetAsync(int month, int year)
-        {
-            throw new NotImplementedException();
-        }
+    public async Task<Budget?> GetBudgetAsync(int month, int year)
+    {
+        Guard.AgainstInvalidMonth(month, nameof(month));
+        Guard.AgainstInvalidYear(year, nameof(year));
 
-        public Task<Budget?> GetBudgetCompleteAsync(Guid budgetId)
-        {
-            throw new NotImplementedException();
-        }
+        return await _context.Budgets.AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Month == month && x.Year == year).ConfigureAwait(false);
+    }
 
-        public Task<Budget?> GetBudgetWithCategoriesAsync(Guid budgetId)
-        {
-            throw new NotImplementedException();
-        }
+    public async Task<bool> BudgetExistsAsync(int month, int year)
+    {
+        Guard.AgainstInvalidMonth(month, nameof(month));
+        Guard.AgainstInvalidYear(year, nameof(year));
 
-        public Task<Budget?> GetByIdAsync(Guid id)
-        {
-            throw new NotImplementedException();
-        }
+        return await _context.Budgets.AnyAsync(x => x.Month == month && x.Year == year).ConfigureAwait(false);
+    }
 
-        public Task UpdateAsync(Budget entity)
-        {
-            throw new NotImplementedException();
-        }
+    public async Task<Budget?> GetBudgetWithCategoriesAsync(Guid budgetId)
+    {
+        Guard.AgainstNull(budgetId, nameof(budgetId));
+
+        return await _context.Budgets.Include(x => x.BudgetCategories)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Id == budgetId).ConfigureAwait(false);
+    }
+
+    public async Task<Budget?> GetBudgetCompleteAsync(Guid budgetId)
+    {
+        Guard.AgainstNull(budgetId, nameof(budgetId));
+
+        return await _context.Budgets.Include(x => x.BudgetCategories)
+            .ThenInclude(x => x.Expenses)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Id == budgetId).ConfigureAwait(false);
     }
 }
