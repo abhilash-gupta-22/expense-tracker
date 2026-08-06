bdae19fe90683de7aceca1f25c6a15f0a42bca18
Abhilash Gupta <abhilash.gupta22@gmail.com>
2026-08-07 00:13:54 +0530

    Implemented Category and Expense repository

--- ExpenseTracker.API/Repositories/CategoryRepository.cs
+++ ExpenseTracker.API/Repositories/CategoryRepository.cs
@@ -1,53 +1,95 @@
-﻿using ExpenseTracker.Domain.Entities;
+﻿using ExpenseTracker.API.Data;
+using ExpenseTracker.Domain.Common;
+using ExpenseTracker.Domain.Entities;
 using ExpenseTracker.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
-namespace ExpenseTracker.API.Repositories
+namespace ExpenseTracker.API.Repositories;
+
+public class CategoryRepository : ICategoryRepository
 {
-    public class CategoryRepository : ICategoryRepository
+    private readonly ExpenseTrackerDbContext _context;
+
+    public CategoryRepository(ExpenseTrackerDbContext context)
     {
-        public Task AddAsync(BudgetCategory entity)
+        Guard.AgainstNull(context, nameof(context));
+
+        _context = context;
+    }
+
+    public async Task<IEnumerable<BudgetCategory>> GetAllAsync()
+    {
+        return await _context.Categories.AsNoTracking().OrderBy(x => x.Name).ToListAsync().ConfigureAwait(false);
+    }
+
+    public async Task<BudgetCategory?> GetByIdAsync(Guid id)
+    {
+        Guard.AgainstNull(id, nameof(id));
+
+        return await _context.Categories.AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Id == id).ConfigureAwait(false);
+    }
+
+    public async Task AddAsync(BudgetCategory entity)
+    {
+        Guard.AgainstNull(entity, nameof(entity));
+
+        await _context.Categories.AddAsync(entity).ConfigureAwait(false);
+        await _context.SaveChangesAsync().ConfigureAwait(false);
+    }
+
+    public async Task UpdateAsync(BudgetCategory entity)
+    {
+        Guard.AgainstNull(entity, nameof(entity));
+
+        _context.Categories.Update(entity);
+        await _context.SaveChangesAsync().ConfigureAwait(false);
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        Guard.AgainstNull(id, nameof(id));
+
+        var category = await _context.Categories.FindAsync(id).ConfigureAwait(false);
+
+        if (category is null)
         {
-            throw new NotImplementedException();
+            return;
         }
 
-        public Task<bool> CategoryExistsAsync(Guid budgetId, string categoryName)
-        {
-            throw new NotImplementedException();
-        }
+        _context.Categories.Remove(category);
 
-        public Task DeleteAsync(Guid id)
-        {
-            throw new NotImplementedException();
-        }
+        await _context.SaveChangesAsync().ConfigureAwait(false);
+    }
 
-        public Task<bool> ExistsAsync(Guid id)
-        {
-            throw new NotImplementedException();
-        }
+    public async Task<bool> ExistsAsync(Guid id)
+    {
+        Guard.AgainstNull(id, nameof(id));
 
-        public Task<IEnumerable<BudgetCategory>> GetAllAsync()
-        {
-            throw new NotImplementedException();
-        }
+        return await _context.Categories.AnyAsync(x => x.Id == id).ConfigureAwait(false);
+    }
 
-        public Task<BudgetCategory?> GetByIdAsync(Guid id)
-        {
-            throw new NotImplementedException();
-        }
+    public async Task<IEnumerable<BudgetCategory>> GetCategoriesByBudgetAsync(Guid budgetId)
+    {
+        Guard.AgainstNull(budgetId, nameof(budgetId));
 
-        public Task<IEnumerable<BudgetCategory>> GetCategoriesByBudgetAsync(Guid budgetId)
-        {
-            throw new NotImplementedException();
-        }
+        return await _context.Categories.Where(x => x.BudgetId == budgetId).OrderBy(x => x.Name)
+            .AsNoTracking().ToListAsync().ConfigureAwait(false);
+    }
 
-        public Task<BudgetCategory?> GetCategoryWithExpensesAsync(Guid categoryId)
-        {
-            throw new NotImplementedException();
-        }
+    public async Task<BudgetCategory?> GetCategoryWithExpensesAsync(Guid categoryId)
+    {
+        Guard.AgainstNull(categoryId, nameof(categoryId));
 
-        public Task UpdateAsync(BudgetCategory entity)
-        {
-            throw new NotImplementedException();
-        }
+        return await _context.Categories.Include(x => x.Expenses).AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Id == categoryId).ConfigureAwait(false);
+    }
+
+    public async Task<bool> CategoryExistsAsync(Guid budgetId, string categoryName)
+    {
+        Guard.AgainstNull(budgetId, nameof(budgetId));
+        Guard.AgainstNull(categoryName, nameof(categoryName));
+
+        return await _context.Categories.AnyAsync(x => x.BudgetId == budgetId && x.Name == categoryName).ConfigureAwait(false);
     }
 }
--- ExpenseTracker.API/Repositories/ExpenseRepository.cs
+++ ExpenseTracker.API/Repositories/ExpenseRepository.cs
@@ -1,53 +1,101 @@
-﻿using ExpenseTracker.Domain.Entities;
+﻿using ExpenseTracker.API.Data;
+using ExpenseTracker.Domain.Common;
+using ExpenseTracker.Domain.Entities;
 using ExpenseTracker.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
-namespace ExpenseTracker.API.Repositories
+namespace ExpenseTracker.API.Repositories;
+
+public class ExpenseRepository : IExpenseRepository
 {
-    public class ExpenseRepository : IExpenseRepository
+    private readonly ExpenseTrackerDbContext _context;
+
+    public ExpenseRepository(ExpenseTrackerDbContext context)
     {
-        public Task AddAsync(Expense entity)
+        Guard.AgainstNull(context, nameof(context));
+
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Expense>> GetAllAsync()
+    {
+        return await _context.Expenses.Include(x => x.BudgetCategoryName)
+            .AsNoTracking().OrderByDescending(x => x.ExpenseDate)
+            .ToListAsync().ConfigureAwait(false);
+    }
+
+    public async Task<Expense?> GetByIdAsync(Guid id)
+    {
+        return await _context.Expenses.Include(x => x.BudgetCategoryName)
+            .AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task AddAsync(Expense entity)
+    {
+        Guard.AgainstNull(entity, nameof(entity));
+
+        await _context.Expenses.AddAsync(entity).ConfigureAwait(false);
+        await _context.SaveChangesAsync().ConfigureAwait(false);
+    }
+
+    public async Task UpdateAsync(Expense entity)
+    {
+        Guard.AgainstNull(entity, nameof(entity));
+
+        _context.Expenses.Update(entity);
+        await _context.SaveChangesAsync().ConfigureAwait(false);
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        Guard.AgainstNull(id, nameof(id));
+
+        var expense = await _context.Expenses.FindAsync(id).ConfigureAwait(false);
+
+        if (expense is null)
         {
-            throw new NotImplementedException();
+            return;
         }
 
-        public Task DeleteAsync(Guid id)
-        {
-            throw new NotImplementedException();
-        }
+        _context.Expenses.Remove(expense);
+        await _context.SaveChangesAsync().ConfigureAwait(false);
+    }
 
-        public Task<bool> ExistsAsync(Guid id)
-        {
-            throw new NotImplementedException();
-        }
+    public async Task<bool> ExistsAsync(Guid id)
+    {
+        Guard.AgainstNull(id, nameof(id));
 
-        public Task<IEnumerable<Expense>> GetAllAsync()
-        {
-            throw new NotImplementedException();
-        }
+        return await _context.Expenses.AnyAsync(x => x.Id == id).ConfigureAwait(false);
+    }
 
-        public Task<Expense?> GetByIdAsync(Guid id)
-        {
-            throw new NotImplementedException();
-        }
+    public async Task<IEnumerable<Expense>> GetExpensesByCategoryAsync(Guid categoryId)
+    {
+        Guard.AgainstNull(categoryId, nameof(categoryId));
 
-        public Task<IEnumerable<Expense>> GetExpensesByCategoryAsync(Guid categoryId)
-        {
-            throw new NotImplementedException();
-        }
+        return await _context.Expenses.Include(x => x.BudgetCategoryName)
+            .Where(x => x.BudgetCategoryId == categoryId)
+            .OrderByDescending(x => x.ExpenseDate)
+            .AsNoTracking()
+            .ToListAsync().ConfigureAwait(false);
+    }
 
-        public Task<IEnumerable<Expense>> GetExpensesByDateRangeAsync(DateTime from, DateTime to)
-        {
-            throw new NotImplementedException();
-        }
+    public async Task<IEnumerable<Expense>> GetExpensesByDateRangeAsync(DateTime from, DateTime to)
+    {
+        Guard.AgainstNull(from, nameof(from));
+        Guard.AgainstNull(to, nameof(to));
 
-        public Task<decimal> GetTotalExpenseAsync(Guid categoryId)
-        {
-            throw new NotImplementedException();
-        }
+        return await _context.Expenses.Include(x => x.BudgetCategoryName)
+            .Where(x => x.ExpenseDate >= from && x.ExpenseDate <= to)
+            .OrderByDescending(x => x.ExpenseDate)
+            .AsNoTracking()
+            .ToListAsync().ConfigureAwait(false);
+    }
 
-        public Task UpdateAsync(Expense entity)
-        {
-            throw new NotImplementedException();
-        }
+    public async Task<decimal> GetTotalExpenseAsync(Guid categoryId)
+    {
+        Guard.AgainstNull(categoryId, nameof(categoryId));
+
+        return await _context.Expenses.Where(x => x.BudgetCategoryId == categoryId)
+            .SumAsync(x => x.Amount).ConfigureAwait(false);
     }
 }
