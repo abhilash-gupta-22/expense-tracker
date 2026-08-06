8c530e5ac65c909477701086153e963f7d4f5c2b
Abhilash Gupta <abhilash.gupta22@gmail.com>
2026-08-07 00:17:44 +0530

    Registered repositories

--- ExpenseTracker.API/Program.cs
+++ ExpenseTracker.API/Program.cs
@@ -1,3 +1,6 @@
+using ExpenseTracker.API.Repositories;
+using ExpenseTracker.Domain.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Register repositories
+builder.Services.AddScoped<IBudgetRepository, BudgetRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IExpenseRepository, ExpenseRepository>();
 
 var app = builder.Build();
 
