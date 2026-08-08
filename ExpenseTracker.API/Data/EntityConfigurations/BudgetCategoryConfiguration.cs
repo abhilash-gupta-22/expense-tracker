7a73562affbb17f092820329ad00dc021cff9a2d
Abhilash Gupta <abhilash.gupta22@gmail.com>
2026-08-08 23:58:59 +0530

    Updated Budget and BudgetCategories Configuration

--- ExpenseTracker.API/Data/EntityConfigurations/BudgetCategoryConfiguration.cs
+++ ExpenseTracker.API/Data/EntityConfigurations/BudgetCategoryConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using ExpenseTracker.Domain.Entities;
+﻿using ExpenseTracker.Domain.Common;
+using ExpenseTracker.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -8,6 +9,22 @@ public class BudgetCategoryConfiguration : IEntityTypeConfiguration<BudgetCatego
 {
     public void Configure(EntityTypeBuilder<BudgetCategory> builder)
     {
-        throw new NotImplementedException();
+        builder.ToTable("BudgetCategories");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id).ValueGeneratedNever();
+
+        builder.Property(x => x.BudgetId).IsRequired();
+
+        builder.Property(x => x.Name).IsRequired().HasMaxLength(DomainConstants.MaxCategoryNameLength);
+
+        builder.Property(x => x.AllocatedBudget).IsRequired().HasPrecision(18, 2);
+
+        builder.HasIndex(x => new
+        {
+            x.BudgetId,
+            x.Name
+        }).IsUnique();
     }
 }
--- ExpenseTracker.API/Data/EntityConfigurations/BudgetConfiguration.cs
+++ ExpenseTracker.API/Data/EntityConfigurations/BudgetConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using ExpenseTracker.Domain.Entities;
+﻿using ExpenseTracker.Domain.Common;
+using ExpenseTracker.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -8,6 +9,27 @@ public class BudgetConfiguration : IEntityTypeConfiguration<Budget>
 {
     public void Configure(EntityTypeBuilder<Budget> builder)
     {
-        throw new NotImplementedException();
+        builder.ToTable("Budgets");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id).ValueGeneratedNever();
+
+        builder.Property(x => x.Month).IsRequired();
+
+        builder.Property(x => x.Year).IsRequired();
+
+        builder.Property(x => x.TotalBudget).IsRequired().HasPrecision(18, 2);
+
+        builder.HasIndex(x => new
+        {
+            x.Month,
+            x.Year
+        }).IsUnique();
+
+        builder.HasMany(x => x.BudgetCategories)
+            .WithOne()
+            .HasForeignKey(x => x.BudgetId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
