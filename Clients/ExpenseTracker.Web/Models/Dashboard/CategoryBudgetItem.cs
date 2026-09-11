d5de06261ce4faa88348a59f88d8956302b7682c
Abhilash Gupta <abhilash.gupta22@gmail.com>
2026-09-11 23:55:58 +0530

    Updated Dashboard razor

---
+++ Clients/ExpenseTracker.Web/Models/Dashboard/CategoryBudgetItem.cs
@@ -0,0 +1,12 @@
+﻿namespace ExpenseTracker.Web.Models.Dashboard;
+
+public class CategoryBudgetItem
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public decimal AllocatedBudget { get; set; }
+
+    public decimal SpentAmount { get; set; }
+}
